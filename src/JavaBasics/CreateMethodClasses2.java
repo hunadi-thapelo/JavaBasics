edecc8e8444fca28dcdd33d36d594d547f9df2f9
hunadim <hmapulane@gmail.com>
2022-11-29 22:16:12 +0200

    Create own method class

--- src/JavaBasics/CreateMethodClasses2.java
+++ src/JavaBasics/CreateMethodClasses2.java
@@ -5,6 +5,6 @@ public class CreateMethodClasses2
         public void getUserData()
         { //to return a value - specify the data type e.g int in place of void
 
-            System.out.println("My new second method");
+            System.out.println("My new second method being called");
         }
 }
