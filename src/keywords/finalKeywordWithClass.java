7231dee9beecc1f0690417d6f8496608e5aa930a
hunadim <hmapulane@gmail.com>
2022-12-06 22:57:43 +0200

    interview ques practice: final keyword with class

---
+++ src/keywords/finalKeywordWithClass.java
@@ -0,0 +1,21 @@
+package keywords;
+
+final class finalKeywordWithClass {
+    public static void main(String[] args)
+    {
+        //important interview question
+        //final is reserved keyword in java
+        //can be used with variables, methods, classes
+        //final classes prevents inheritance
+
+        int a = 20;
+        System.out.println(a);
+
+    }
+
+    //extending super-class (parent class) will give compile time error
+    //class newClass extends finalKeywordWithClass
+    //{
+
+    //}
+}
