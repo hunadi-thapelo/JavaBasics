e5fa8f3f9fa5c294615b6b2aba84c1e65d7a4b90
hunadim <hmapulane@gmail.com>
2022-12-12 23:45:45 +0200

    intro static concept: to save memory with static keyword in province instance variable

--- src/oopsInJava/staticVar.java
+++ src/oopsInJava/staticVar.java
@@ -5,27 +5,31 @@ public class staticVar {
     //instance variables and class variables
     String suburb;
     String city;
-    String province;
+    static String province = "Johannesburg"; //change property to static property to be shared by object in main class
 
     //local variables
-    staticVar(String suburb, String city, String province)
+    staticVar(String suburb, String city)
     {
         //assign local variables to instance variables
         this.suburb = suburb;//use operator this to identify parameters to this class
         this.city = city;
-        this.province = province;
+        //this.province = province; // remove this variable to match number of parameters in constructor
     }
 
     //create method of this class 'staticVar'
     public void getSuburb(){
 
-        System.out.println(suburb);
+        System.out.println(suburb + " " + province); //print province variable
     }
 
     public static void main(String[] args){
         //instance variables or variables in the class are activated by values by what we send from object
-        staticVar obj = new staticVar("Ferndale", "Johannesburg North", "Gauteng");
-        staticVar obj1 = new staticVar("Oakdene", "Johannesburg South", "Gauteng");
+        //staticVar obj = new staticVar("Ferndale", "Johannesburg North", "Gauteng");
+        staticVar obj = new staticVar("Ferndale", "Johannesburg North"); //remove parameter Gauteng, it is the same value
+                                                                                    // - but this save memory in Java
+        //staticVar obj1 = new staticVar("Oakdene", "Johannesburg South", "Gauteng");
+        staticVar obj1 = new staticVar("Oakdene", "Johannesburg South"); //same for this object - refer to static instance variable
+
         obj.getSuburb(); //print method value of this object
         obj1.getSuburb();
 
