5c72ee71759c06f7187d783c3064d27c7fdc3e2e
hunadim <hmapulane@gmail.com>
2022-12-16 22:31:44 +0200

    Working with SimpleDateFormat user-defined patterns for date-time formatting

--- src/JavaBasics/dateDemo.java
+++ src/JavaBasics/dateDemo.java
@@ -1,12 +1,18 @@
 package JavaBasics;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class dateDemo {
     public static void main(String[] args)
     {
         Date d = new Date();
+        SimpleDateFormat customDate = new SimpleDateFormat("M/dd/yyyy"); //customise my own date format
+        SimpleDateFormat customDateTime = new SimpleDateFormat("M/dd/yyyy hh:mm:ss");
         System.out.println(d); //2022: toString method to convert date object into readable text found redundant
+        System.out.println(customDate.format(d));
+        System.out.println(customDateTime.format(d));
+
 
     }
 }
