44c9dd93f41aaac074aaf402961f28db02e14bd9
hunadim <hmapulane@gmail.com>
2022-12-16 22:16:49 +0200

    working with date class concept

---
+++ src/JavaBasics/dateDemo.java
@@ -0,0 +1,12 @@
+package JavaBasics;
+
+import java.util.Date;
+
+public class dateDemo {
+    public static void main(String[] args)
+    {
+        Date d = new Date();
+        System.out.println(d); //2022: toString method to convert date object into readable text found redundant
+
+    }
+}
