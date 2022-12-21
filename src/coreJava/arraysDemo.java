15803dda07d3098cdb735a59332c4aa355c69451
hunadim <hmapulane@gmail.com>
2022-12-21 10:11:35 +0200

    arraysDemo in Core Java section - WIP

---
+++ src/coreJava/arraysDemo.java
@@ -0,0 +1,30 @@
+package coreJava;
+
+public class arraysDemo {
+    String name, lastname;
+
+    //getters
+   public String getName(){
+        return name;
+    }
+    public String getLastname(){
+       return lastname;
+    }
+
+    //setters
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public void setLastname(String newLastname){
+       lastname = newLastname;
+    }
+
+    public static void main(String[] args)
+    {
+
+
+
+    }
+
+}
