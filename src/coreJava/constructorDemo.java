a068d2b50b744e2b8afd6f63c729c0c7fa4662e2
hunadim <hmapulane@gmail.com>
2023-01-06 22:08:46 +0200

    Practice - access modifiers

--- src/coreJava/constructorDemo.java
+++ src/coreJava/constructorDemo.java
@@ -5,6 +5,7 @@ public class constructorDemo {
     //has not return type example 'void' like a method
     //NB: name of constructor should be class name
     public constructorDemo() //defined constructor - code in the block{} will be executed in main class
+    //public void constructorDemo() //with 'void' method is not executed when accessed by another class e.g accessModifier class
     {
         System.out.println("I am a constructor!");
         System.out.println("I am the second constructor!");
@@ -15,13 +16,17 @@ public class constructorDemo {
         System.out.println("I am the method");
     }
 
+    void myABC(){
+        System.out.println("I am the default method from constructorDemo class/program");
+    }
+
     public static void main(String[] args)
     {
         //constructor is a block of code when object is created/instantiated
         //compiler calls implicit constructor when constructor is not defined
         constructorDemo demoThis=new constructorDemo();
         demoThis.getData(); //calls class method
-
+        //demoThis.constructorDemo();
     }
 
 }
