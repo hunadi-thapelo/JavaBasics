d55de4c518853b96b42f15722098df631de01128
hunadim <hmapulane@gmail.com>
2023-02-10 21:23:11 +0200

    applies interface public variable

--- src/coreJava/CentralTraffic.java
+++ src/coreJava/CentralTraffic.java
@@ -2,6 +2,8 @@ package coreJava;
 
 public interface CentralTraffic {
 
+    int a = 4; //interface variables are 'public' by default e.g public int a = 4;
+
     public void GreenGo();
     public void YellowWait();
     public void RedStop();
