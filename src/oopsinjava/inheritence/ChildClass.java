ef93828fe29fa49c119146f7eb35c0ae30cd5012
hunadim <hmapulane@gmail.com>
2023-03-05 21:50:44 +0200

    implements override concept via capture screen method

--- src/oopsinjava/inheritence/ChildClass.java
+++ src/oopsinjava/inheritence/ChildClass.java
@@ -7,13 +7,19 @@ public class ChildClass extends ParentClass{ //childclass inherits parentclass m
         System.out.println("new hd camera (from child class)");
     }
 
+    public void captureScreen(){
+
+        System.out.println("child capture screen implementation");
+    }
+
     public static void main(String[] args) {
 
         ChildClass cc = new ChildClass();
         System.out.println(cc.label);
         cc.answerCall();
         cc.camera();
+        cc.captureScreen(); //method override implemented //output from child class
+                            //give preference to local captureScreen method only
     }
-
 
 }
